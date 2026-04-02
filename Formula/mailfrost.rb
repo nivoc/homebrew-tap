8b0f11b83b8b07855aeae564a337afdbdb486958
Matthias Kuehnle <kuehnle@online.de>
2026-04-02 13:26:43 +0200

    Add Mailfrost formula

---
+++ Formula/mailfrost.rb
@@ -0,0 +1,20 @@
+class Mailfrost < Formula
+  desc "Maildir integrity tracking, confidence, and backup"
+  homepage "https://github.com/nivoc/mailfrost"
+  url "https://github.com/nivoc/mailfrost/releases/download/v0.1.4/mailfrost_v0.1.4_macos_apple_silicon.zip"
+  sha256 "2b263dbb0940ad630fdcb585cd919ea612742797f17037b87383158ede2f810a"
+  version "0.1.4"
+
+  depends_on arch: :arm64
+
+  def install
+    odie "Mailfrost is currently packaged only for macOS Apple Silicon." unless OS.mac? && Hardware::CPU.arm?
+
+    bin.install "mailfrost"
+    prefix.install "README.txt"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mailfrost version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-tap
+
+Homebrew tap for nivoc tools.
+
+Install Mailfrost:
+
+```bash
+brew tap nivoc/tap
+brew install mailfrost
+```
